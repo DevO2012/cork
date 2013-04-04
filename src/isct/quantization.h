420d9c920ecebc9be7386808d6e87bf811685be3
C++1y <DevOmem@web.de>
2013-04-04 15:19:33 +0300

    Update quantization.h
    turn quantization into a Quantizator class

--- src/isct/quantization.h
+++ src/isct/quantization.h
@@ -27,39 +27,44 @@
 
 #include<cmath>
 
-namespace Quantization {
 
-// NOTE: none of these values should be modified by the clients
-static const int BITS = 30;
-// MAGNIFY * RESHRINK == 1
-extern double MAGNIFY;
-extern double RESHRINK;
-inline int quantize2int(double number) {
-    return int(number * MAGNIFY);
-}
-inline double quantizedInt2double(int number) {
-    return RESHRINK * double(number);
-}
-inline double quantize(double number) {
-    return RESHRINK * double(int(number * MAGNIFY));
-}
-
-// given the specified number of bits,
-// and bound on the coordinate values of points,
-// fit as fine-grained a grid as possible over the space.
-inline void callibrate(double maximumMagnitude)
+class Quantizator 
 {
-    int max_exponent;
-    std::frexp(maximumMagnitude, &max_exponent);
-    max_exponent++; // ensure that 2^max_exponent > maximumMagnitude
-    
-    // set constants
-    MAGNIFY = std::pow(2.0, BITS - max_exponent);
-    // we are guaranteed that maximumMagnitude * MAGNIFY < 2.0^BITS
-    RESHRINK = std::pow(2.0, max_exponent - BITS);
-}
+public:
+    // NOTE: none of these values should be modified by the clients
+	static const int BITS = 30; //this is for Int32 !!!
 
+	// m_magnify * m_reshrink == 1
+	double m_magnify;
+	double m_reshrink;
 
-} // end namespace Quantization
+public:
+
+	inline int quantize2int(double number) const {
+		return int(number * m_magnify);
+	}
+	inline double quantizedInt2double(int number) const {
+		return m_reshrink * double(number);
+	}
+	inline double quantize(double number) const {
+		return m_reshrink * double(int(number * m_magnify));
+	}
+
+	// given the specified number of bits,
+	// and bound on the coordinate values of points,
+	// fit as fine-grained a grid as possible over the space.
+	inline void callibrate(double maximumMagnitude)
+	{
+		int max_exponent;
+		std::frexp(maximumMagnitude, &max_exponent);
+		max_exponent++; // ensure that 2^max_exponent > maximumMagnitude
+
+		// set constants
+		m_magnify = std::pow(2.0, BITS - max_exponent);
+		// we are guaranteed that maximumMagnitude * m_magnify < 2.0^BITS
+		m_reshrink = std::pow(2.0, max_exponent - BITS);
+
+	}
+};
 
 
